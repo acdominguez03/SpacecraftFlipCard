f0a294d5264adb606ee1c56032e02eabbcdb06d7
acdominguez03 <acordondominguez31@gmail.com>
2025-03-27 12:28:14 +0100

    Initial Commit

---
+++ SpacecraftFlipCard/SpacecraftFlipCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpacecraftFlipCardApp.swift
+//  SpacecraftFlipCard
+//
+//  Created by Andres Cordón on 27/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SpacecraftFlipCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
